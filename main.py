4957b9c7aacd57be189e8ce5b817de16999392b1
Malcolm Sng <malcolmsng99@gmail.com>
2023-01-30 17:15:09 +0800

    added sample data & trying to read

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 #Hello World
 #all functions to be executed here
+#trying stuff out
+import pandas as pd
+
+sample_data = pd.read_csv("./data/sample_data.csv", skiprows= 1)
+df = pd.DataFrame(sample_data)
+print(df.head())
