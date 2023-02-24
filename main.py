03ee0d787f64a3bc89d41f057c6288f23fdd2d6a
Malcolm Sng <malcolmsng99@gmail.com>
2023-02-24 14:08:04 +0800

    settings window completed

--- main.py
+++ main.py
@@ -9,44 +9,92 @@ import pandas as pd
 import PySimpleGUI as sg
 
 
-def add_row_window():
-
-
-    layout = [[sg.T("Placeholder")]]
+def settings_window(settings):
+    layout = [[sg.T("SETTINGS")],
+              [sg.T("Font Size:"), sg.I(settings["GUI"]
+                                        ["font_size"], s=20, key="-FONT_SIZE-")],
+              [sg.T("Font Family:"), sg.I(settings["GUI"]
+                                          ["font_family"], s=20, key="-FONT_FAMILY-")],
+              [sg.T("Theme:"), sg.I(settings["GUI"]
+                                    ["theme"], s=20, key="-THEME-")],
+              [sg.T("Sheet Name:"), sg.I(settings["EXCEL"]
+                                         ["sheet_name"], s=20, key="-SHEET_NAME-")],
+              [sg.B("Save Current Settings", s=20)]]
     window = sg.Window("Settings Window", layout,
                        modal=True, use_custom_titlebar=True)
     while True:
         event, values = window.read()
         if event == sg.WINDOW_CLOSED:
             break
-    
+        if event == "Save Current Settings":
+            # Write to ini file
+            settings["EXCEL"]["sheet_name"] = values["-SHEET_NAME-"]
+            settings["GUI"]["font_size"] = values["-FONT_SIZE-"]
+            settings["GUI"]["font_family"] = values["-FONT_FAMILY-"]
+            settings["GUI"]["theme"] = values["-THEME-"]
+
+            # Display success message & close window
+            sg.popup_no_titlebar("Settings saved!")
+            break
     window.close()
 
 
+def add_row_window():
+
+    layout = [[sg.T("Placeholder")], [[sg.T("Input :", s=15, justification="r"), sg.I(
+        key="-IN-"), sg.FileBrowse(file_types=(("Excel Files", "*.xls*"),))], ]]
+    window = sg.Window("Add Row to Excel", layout,
+                       modal=True, use_custom_titlebar=True)
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED:
+            break
+
+    window.close()
 
 
 def excel_to_csv_window():
-    return
+    layout = [[sg.T("Placeholder")],
+              [sg.T("Input :", s=15, justification="r"), sg.I(
+                  key="-IN-"), sg.FileBrowse(file_types=(("Excel Files", "*.xls*"),)), ],
+              [sg.T("Output Folder:", s=15, justification="r"),
+               sg.I(key="-OUT-"), sg.FolderBrowse()],
+              [sg.T("Rows To Add :", s=15, justification="r"), sg.I(default_text="XX:YY",
+                                                                    key="-ROWS-", s=8),
+              sg.B("Add Rows", s=16), ]]
+    window = sg.Window("Add Row to Excel", layout,
+                       modal=True, use_custom_titlebar=True)
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED:
+            break
+
+    window.close()
 
 
 def main_window():
     # GUI Definition
     layout = [
-        # [sg.T("Input :", s=15, justification="r"), sg.I(
-        #     key="-IN-"), sg.FileBrowse(file_types=(("Excel Files", "*.xls*"),))],
-        # [sg.T("Output Folder:", s=15, justification="r"),
-        #  sg.I(key="-OUT-"), sg.FolderBrowse()],
-        [sg.B("Settings", s=16), sg.B("Add Rows To Excel", s=16), sg.B("Excel Sheet To CSV", s=16)], ]
+        [
+            sg.B("Settings", s=16),
+            sg.B("Add Rows To Excel", s=16),
+            sg.B("Excel To CSV", s=16),
+            sg.B("Analyse Data", s=16),
+        ],
+
+    ]
     # window_title = settings["GUI"]["title"]
     window = sg.Window("Temp title", layout)
     while True:
         event, values = window.read()
         if event == sg.WINDOW_CLOSED:
             break
+        if event == "Settings":
+            settings_window(settings)
         if event == "Add Rows To Excel":
             add_row_window()
         if event == "Excel To CSV":
-            sg.popup_error("Not yet implemented")
+            excel_to_csv_window()
 
     window.close()
 
@@ -60,6 +108,8 @@ def process_csv(csv_data):
     return
 # convert xlsx to csv format
 
+# add excel rows/ sheet to csv data file
+
 
 def excel_to_csv(excel_data):
     xlsx_source = f"./data/{excel_data}"
@@ -70,6 +120,15 @@ def excel_to_csv(excel_data):
 
 if __name__ == '__main__':
     cwd = os.getcwd()
+    # cosmetics for GUI
+    settings = sg.UserSettings(
+        path=cwd, filename="config.ini", use_config_file=True, convert_bools_and_none=True
+    )
+    theme = settings["GUI"]["theme"]
+    font_family = settings["GUI"]["font_family"]
+    font_size = int(settings["GUI"]["font_size"])
+    sg.theme(theme)
+    sg.set_options(font=(font_family, font_size))
     # path = os.path.join(cwd, "data")
     # data = os.listdir(path)
     # excel_data = list(filter(lambda f: f.endswith('.xlsx'), data))
